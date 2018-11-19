8cd89a7046dd6e4955e8ee0a40184474a4bd51bd
Tianchi Xu <xtc459924276@gmail.com>
2018-11-19 01:20:15 -0500

    Add a sphere

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-let scene,camera,renderer;
-
+let scene, camera, renderer;
 
 function init() {
   // listen to the resize events
@@ -9,43 +8,65 @@ function init() {
   scene = new THREE.Scene();
 
   // create a camera, which defines where we're looking at.
-  camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
 
   // create a render and set the size
   renderer = new THREE.WebGLRenderer();
   renderer.setClearColor(new THREE.Color(0x000000));
   renderer.setSize(window.innerWidth, window.innerHeight);
 
-
-    // show axes in the screen
+  // show axes in the screen
   var axes = new THREE.AxesHelper(30);
   scene.add(axes);
 
-  let planeGeometry = new THREE.PlaneGeometry(50, 50);
-  let planeMaterial = new THREE.MeshBasicMaterial({
-    color: 0xAAAAAA
-  });
-  let plane = new THREE.Mesh(planeGeometry, planeMaterial);
-  // rotate and position the plane
-  plane.rotation.x = -0.5 * Math.PI;
-  plane.position.set(0, 0, 0);
-
-  // add the plane to the scene
-  scene.add(plane);
-
+  addPlane();
+  addSphere();
+  
   // position and point the camera to the center of the scene
   camera.position.set(-30, 40, 45);
   camera.lookAt(scene.position);
 
-   // add the output of the renderer to the html element
-   document.getElementById("three-output").appendChild(renderer.domElement);
+  // add the output of the renderer to the html element
+  document.getElementById("three-output").appendChild(renderer.domElement);
 
-   // render the scene
-   renderer.render(scene, camera);
+  // render the scene
+  renderer.render(scene, camera);
 }
 
 function onResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function addPlane() {
+  let planeGeometry = new THREE.PlaneGeometry(50, 50);
+  let planeMaterial = new THREE.MeshBasicMaterial({
+    color: 0xaaaaaa
+  });
+  let plane = new THREE.Mesh(planeGeometry, planeMaterial);
+  // rotate and position the plane
+  plane.rotation.x = -0.5 * Math.PI;
+  plane.position.set(10, 0, 0);
+
+  // add the plane to the scene
+  scene.add(plane);
+}
+
+function addSphere() {
+  // create a sphere
+  var sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
+  var sphereMaterial = new THREE.MeshBasicMaterial({ color: 0x7777ff });
+  var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+
+  // position the sphere
+  sphere.position.set(0, 5, 10);
+
+  // add the sphere to the scene
+  scene.add(sphere);
 }
