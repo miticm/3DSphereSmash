51b744cb3acc3f4a1fa5e00f4837ff35a22a5599
Elijah <elijahs2106@gmail.com>
2018-12-05 00:34:11 -0500

    Add functionality for more than 2 users to the backend

--- server.js
+++ server.js
@@ -1,54 +1,90 @@
 const express = require('express');
 const socketio = require("socket.io");
+const _ = require("underscore");
 let players = [];
+let pendings = [];
+let matches = {};
 
 const app = express();
 app.use(express.static("src"));
+
 app.get('/',(req,res)=>{
   res.sendfile("./src/index.html");
-})
+});
+
 const server = app.listen(8888, () => {
   console.log("http://localhost:8888");
 });
 
 const io = socketio(server);
 
-io.on("connect",socket=>{
+io.on("connect", socket => {
   console.log(`Connected with ${socket.id}`);
-  let me = new player();
+  let me = new player(socket.id, socket);
   players.push(me);
-  let pIndex = players.length - 1
+
+  var id = _.sample(pendings);
+  let pIndex = 0;
+  let p2Index = 1;
+
+  if (id) {
+    pIndex = 1;
+    p2Index = 0;
+  }
 
   socket.emit('connected',{
     index: pIndex
-  })
-  
-  //console.log(Object.keys(io.sockets.connected));
+  });
 
-  if(io.engine.clientsCount == 2){
-    io.sockets.emit("start");
+  if (!id) {
+    console.log("added " + me.id + " to pending");
+    pendings.push(me.id);
+    me.socket.emit("pending", {status: "pending", message: "waiting for a new player."});
+  }
+  else {
+    pendings = _.without(pendings, id);
+    matches[id] = me.id;
+    matches[me.id] = id;
+    console.log(matches);
+    me.socket.emit("start");
+    playerById(id).socket.emit("start");
   }
 
-  socket.on("disconnect",()=>{
-    console.log(`Disconnected with ${socket.id}`)
-    players = [];
-    io.sockets.emit("disconnect");
-  })
+  socket.on("disconnect", () => {
+    console.log(`Disconnected with ${socket.id}`);
+  });
 
-  socket.on('speed',data=>{
-    if(players.length == 2){
-      players[0].speed = data[0];
-      players[1].speed = data[1];
-      io.sockets.emit("update",{
-        players
-      })
+  socket.on('speed', data => {
+    if (playerById(id) !== false) {
+      me.speed = data[pIndex];
+      playerById(id).speed = data[p2Index];
+      me.socket.emit("update", {
+        me: {speed: me.speed},
+        opponent: {speed:playerById(id)
+      });
+      playerById(id).socket.emit("update", {
+        me: {speed: playerById(id).speed},
+        opponent: {speed: me.speed }
+      });
     }
-  })
+  });
 
-})
+});
 
 class player {
-  constructor(){
-    this.speed = {x:0,y:0,z:0};
+  constructor(id, socket) {
+    this.speed = {x: 0, y:0, z:0};
+    this.id = id;
+    this.socket = socket;
   }
+}
+
+function playerById(id) {
+  var i;
+  for (i = 0; i < players.length; i++) {
+      if (players[i].id == id)
+          return players[i];
+  };
+  
+  return false;
 }
